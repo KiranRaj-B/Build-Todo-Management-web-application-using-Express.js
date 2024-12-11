0f2ab61217c95bd11fcd661dbe20d49d77cb93b6
Kiran Raj <kiranrajbnadakambi@gmail.com>
2024-12-11 12:31:39 +0530

    migrate update

--- migrations/20241209053720-create-todo.js
+++ migrations/20241209053720-create-todo.js
@@ -1,34 +1,35 @@
 'use strict';
-/** @type {import('sequelize-cli').Migration} */
 module.exports = {
-  async up(queryInterface, Sequelize) {
+  up: async (queryInterface, Sequelize) => {
     await queryInterface.createTable('Todos', {
       id: {
         allowNull: false,
         autoIncrement: true,
         primaryKey: true,
-        type: Sequelize.INTEGER
+        type: Sequelize.INTEGER,
       },
       title: {
-        type: Sequelize.STRING
+        type: Sequelize.STRING,
+        allowNull: false,
       },
-      duedate: {
-        type: Sequelize.DATEONLY
+      dueDate: {
+        type: Sequelize.DATEONLY,
       },
       completed: {
-        type: Sequelize.BOOLEAN
+        type: Sequelize.BOOLEAN,
+        defaultValue: false,
       },
       createdAt: {
         allowNull: false,
-        type: Sequelize.DATE
+        type: Sequelize.DATE,
       },
       updatedAt: {
         allowNull: false,
-        type: Sequelize.DATE
-      }
+        type: Sequelize.DATE,
+      },
     });
   },
-  async down(queryInterface, Sequelize) {
+  down: async (queryInterface, Sequelize) => {
     await queryInterface.dropTable('Todos');
-  }
+  },
 };
